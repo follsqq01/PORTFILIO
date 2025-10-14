d9feddccfc6af9faac8dcafaabe10c79c9ed118f
follsqq01 <ruslanmelkumian80@yandex.ru>
2025-10-14 21:07:12 +0300

    FINAL??

--- vite.config.js
+++ vite.config.js
@@ -2,5 +2,5 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
-  base: "/ISCRA_STUDIO/", // обязательно имя репозитория!
+  base: "/PORTFILIO/", // обязательно имя репозитория!
 });
