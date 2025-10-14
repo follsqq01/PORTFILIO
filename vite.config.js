9e51789a0bf7f8436cdc9721c0e14f380095c76b
follsqq01 <ruslanmelkumian80@yandex.ru>
2025-10-14 21:55:20 +0300

    god

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import pluginChecker from "vite-plugin-checker";
 export default defineConfig({
   plugins: [react()],
-  base: "/PORTFILIO/", // обязательно имя репозитория!
+  plugins: [pluginChecker({ typescript: true })],
 });
+
+import pluginChecker from "vite-plugin-checker";
